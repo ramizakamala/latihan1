f7d5c9a087276a7861f392910378b950b776c705
ramizakamala <mizarakamala@gmail.com>
2023-03-15 12:12:00 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main (){ //begin
+int luas, P, L;
+//numeriuc luas, P, L;
+//Display, "Masukkan Panjangnya ; "
+cout << "Masukkan Panjangnya :" ;
+//Accept P
+cin >> P;
+cout << "Masukkan Lebarnya :" ;
+//Accept L
+cin >> L;
+//Compute Luas = P*L
+luas = P*L;
+//Display, " Luasnya : " + Luas
+cout << "Luasnya : " << luas;
+//end
+}
